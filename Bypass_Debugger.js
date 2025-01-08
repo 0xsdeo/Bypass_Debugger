a2d98ba79d3dc63909a1031438d6379010062a57
CC11001100 <CC11001100@qq.com>
2025-01-09 01:30:08 +0800

    feat: 增加对setInterval的支持

--- Bypass_Debugger.js
+++ Bypass_Debugger.js
@@ -9,7 +9,7 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     let temp_eval = eval;
@@ -58,4 +58,33 @@
     }
 
     Function.prototype.constructor.prototype = Function.prototype;
+
+    // hook setInterval 中的debugger
+    const setIntervalHolder = window.setInterval;
+    window.setInterval = function () {
+
+        if (arguments.length > 0) {
+            const functionBodyCode = getFunctionBody(arguments[0].toString());
+            // 复用上面的函数体bypass的逻辑
+            arguments[0] = new Function(functionBodyCode);
+        }
+        return setIntervalHolder.apply(this, arguments);
+    }
+
+    /**
+     * 获取函数的方法体代码。
+     *
+     * @param {Function} fullCode - 要提取方法体的函数的代码
+     * @return {string} - 返回函数的方法体代码。
+     * @throws {TypeError} - 如果传入的参数不是函数，则抛出 TypeError。
+     */
+    function getFunctionBody(fullCode) {
+        // 提取方法体的代码
+        const bodyStart = fullCode.indexOf('{') + 1; // 找到方法体的开始位置
+        const bodyEnd = fullCode.lastIndexOf('}');   // 找到方法体的结束位置
+
+        // 提取并返回方法体的代码
+        return fullCode.slice(bodyStart, bodyEnd).trim();
+    }
+
 })();
